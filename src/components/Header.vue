5920d1c5bccaec2dbfcbd61e3cb6dfe1695b3afd
Jefferson <jefferson.miranda@arctouch.com>
2020-06-21 15:10:47 -0300

    creating header component and adding to home page

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <h1 class="header-title"> {{ title }} </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {
+  @Prop(String) readonly title!: string
+}
+
+</script>
+
+<style lang="sass" scoped>
+@import '@/style/_vars'
+
+.header
+  min-height: 150px
+  display: flex
+  justify-content: center
+  align-items: center
+  background-color: $color-green
+
+  &-title
+    font-size: 4rem
+    color: $color-yellow
+
+</style>
