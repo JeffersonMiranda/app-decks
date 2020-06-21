7f60b63b2659167e15e2093508eab8548775db83
Jefferson <jefferson.miranda@arctouch.com>
2020-06-21 19:05:56 -0300

    creating form input component

---
+++ src/components/CardField.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-field">
+    <label class="card-field-label" for=""> {{ fieldLabel }} </label>
+    <input class="card-field-input" type="text" :placeholder="fieldPlaceholder"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {
+  @Prop(String) readonly fieldLabel!: string
+  @Prop({ default: 'Enter card' }) readonly fieldPlaceholder!: string
+}
+
+</script>
+
+<style lang="sass" scoped>
+@import '@/style/_vars'
+
+.card-field
+  max-width: 250px
+  width: 100%
+  display: flex
+  flex-direction: column
+
+  .card-field-label
+    color: $color-light-grey
+    margin-bottom: 5px
+  
+  .card-field-input
+    width: 100%
+    padding: 10px 5px
+    display: flex
+    flex-direction: column
+    border-radius: 2px
+    border: 1px solid $color-light-grey
+    color: $color-light-grey
+
+</style>
