54acd4b034b59d8f2d73744ce6988f7954b6cbb9
Jefferson <jefferson.miranda@arctouch.com>
2020-06-21 22:47:21 -0300

    creating submit button component

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="button button--blue" type="submit"> {{ buttonText }} </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class SubmitButton extends Vue {
+  @Prop(String) readonly buttonText!: string
+}
+
+</script>
+
+<style lang="sass" scoped>
+@import '@/style/_variables'
+
+.button
+  width: 100%
+  text-align: center
+  padding: 20px
+  border: none
+  border-radius: 20px
+  font-size: 2rem
+  font-weight: bold
+  cursor: pointer
+
+  &--blue
+    background-color: $color-blue
+    color: $color-dark-yellow
+
+</style>
