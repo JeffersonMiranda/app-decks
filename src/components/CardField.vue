93234a36bd3b837dd37514bc92358f83c69853e9
Jefferson <jefferson.miranda@arctouch.com>
2020-06-22 10:17:43 -0300

    Binding card fields with Deck store module

--- src/components/CardField.vue
+++ src/components/CardField.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="card-field">
     <label class="card-field-label" for=""> {{ fieldLabel }} </label>
-    <input class="card-field-input" type="text" :placeholder="fieldPlaceholder"/>
+    <input class="card-field-input" type="text" v-model="cardValue" :placeholder="fieldPlaceholder"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop, PropSync } from 'vue-property-decorator'
+import { namespace } from 'vuex-class'
+
+const Deck = namespace('Deck')
 
 @Component
 export default class CardField extends Vue {
   @Prop(String) readonly fieldLabel!: Number
   @Prop({ default: 'Enter card' }) readonly fieldPlaceholder!: Number
   @Prop(Number) readonly fieldOrder!: Number
+
+  @Deck.Action
+  public addCard!:(payload: any) => void
+
+  public addEmptyCard() {
+    this.addCard({ newCard: '', index: this.fieldOrder });
+  }
+
+  @Deck.Getter
+  public getCardByIndex!:(index: Number) => String
+
+  set cardValue(value: String) {
+    this.addCard({ newCard: value, index: this.fieldOrder })
+  }
+
+  get cardValue(): String {
+    return this.getCardByIndex(this.fieldOrder)
+  }
+
+  created() {
+    this.addEmptyCard();
+  }
 }
 
 </script>
