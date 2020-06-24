e3f2f66d640ae48bf4bc05567c0e57b32e5c5a7c
Jefferson <jefferson.miranda@arctouch.com>
2020-06-23 21:28:15 -0300

    Card block component to render a part of a card

---
+++ src/components/CardBlock.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card-block">
+    <span class="card-block-value"> {{ value }} </span>
+    <img class="card-block-suit" :src="getSuitImage()">
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class CardBlock extends Vue {
+  @Prop() private readonly suit!: String
+  @Prop() private readonly value!: String
+
+  private publicPath = process.env.BASE_URL
+
+  public getSuitImage(): string {
+    return `${this.publicPath}suits/${this.suit}.png`
+  }
+}
+
+</script>
+
+<style lang="sass" scoped>
+@import '@/style/_variables'
+
+.card-block
+  display: flex
+  padding: 10px
+
+  .card-block-value
+    font-size: 30px
+    margin-right: 10px
+
+  .card-block-suit
+    max-width: 20px
+</style>
